63c9fc52e1520dc90adae0276ef4ce2fe951aa62
Pietro Lorefice <pietro.lorefice@gmail.com>
2019-02-22 07:43:57 +0100

    Initial commit

---
+++ src/game.rs
@@ -0,0 +1,164 @@
+use piston_window as pw;
+
+use std::collections::HashMap;
+
+pub const TILE_SIZE: (u32, u32) = (128, 128);
+pub const CELL_SIZE: (u32, u32) = (16, 16);
+
+#[derive(Debug, PartialEq)]
+pub enum CellState {
+    Value(u8),
+    Empty,
+    Bomb,
+}
+
+#[derive(Debug)]
+pub struct Cell {
+    state: CellState,
+    hidden: bool,
+}
+
+pub struct Field {
+    cells: HashMap<(u32, u32), Cell>,
+    mouse: (f64, f64),
+    textures: Vec<pw::G2dTexture>,
+}
+
+impl Field {
+    pub const SIZE: (u32, u32) = (32, 32);
+
+    pub fn new<R: rand::Rng + ?Sized>(
+        rng: &mut R,
+        n: usize,
+        textures: Vec<pw::G2dTexture>,
+    ) -> Field {
+        let mut cells = HashMap::new();
+
+        let bomb_indices =
+            rand::seq::index::sample(rng, (Self::SIZE.0 * Self::SIZE.1) as usize, n).into_vec();
+
+        for idx in 0..(Self::SIZE.0 * Self::SIZE.1) {
+            cells.insert(
+                (idx % Self::SIZE.0, idx / Self::SIZE.0),
+                Cell {
+                    state: if bomb_indices.contains(&(idx as usize)) {
+                        CellState::Bomb
+                    } else {
+                        CellState::Empty
+                    },
+                    hidden: true,
+                },
+            );
+        }
+
+        let mut field = Field {
+            cells,
+            mouse: (0.0, 0.0),
+            textures,
+        };
+
+        for y in 0..Self::SIZE.1 {
+            for x in 0..Self::SIZE.0 {
+                if field.cell_at(x, y).state == CellState::Bomb {
+                    continue;
+                }
+
+                let n_bombs = field
+                    .adjacent_cells(x, y)
+                    .into_iter()
+                    .filter(|c| c.state == CellState::Bomb)
+                    .count();
+
+                if n_bombs > 0 {
+                    field.cell_mut_at(x, y).state = CellState::Value(n_bombs as u8);
+                }
+            }
+        }
+
+        field
+    }
+
+    pub fn cell_at(&self, x: u32, y: u32) -> &Cell {
+        &self.cells[&(x, y)]
+    }
+
+    pub fn cell_mut_at(&mut self, x: u32, y: u32) -> &mut Cell {
+        self.cells.get_mut(&(x, y)).unwrap()
+    }
+
+    fn adjacent_coords(&self, x: u32, y: u32) -> Vec<(u32, u32)> {
+        let (x, y) = (i64::from(x), i64::from(y));
+        let mut v = Vec::with_capacity(8);
+
+        for dy in (y - 1).max(0)..(y + 2).min(Self::SIZE.1.into()) {
+            for dx in (x - 1).max(0)..(x + 2).min(Self::SIZE.0.into()) {
+                if (dx, dy) != (x, y) {
+                    v.push((dx as u32, dy as u32));
+                }
+            }
+        }
+        v
+    }
+
+    fn adjacent_cells(&self, x: u32, y: u32) -> Vec<&Cell> {
+        self.adjacent_coords(x, y)
+            .into_iter()
+            .map(|(x, y)| self.cell_at(x, y))
+            .collect::<Vec<_>>()
+    }
+
+    pub fn render(&self, c: pw::Context, g: &mut pw::G2d) {
+        use pw::Transformed;
+
+        for (&(x, y), cell) in self.cells.iter() {
+            let tex = if cell.hidden {
+                &self.textures[0]
+            } else {
+                match cell.state {
+                    CellState::Bomb => &self.textures[2],
+                    CellState::Empty => &self.textures[3],
+                    CellState::Value(n) => &self.textures[4 + (usize::from(n) - 1)],
+                }
+            };
+
+            pw::image(
+                tex,
+                c.transform
+                    .trans(f64::from(x * CELL_SIZE.0), f64::from(y * CELL_SIZE.1))
+                    .scale(
+                        f64::from(CELL_SIZE.0) / f64::from(TILE_SIZE.0),
+                        f64::from(CELL_SIZE.1) / f64::from(TILE_SIZE.1),
+                    ),
+                g,
+            );
+        }
+    }
+
+    pub fn mouse_move(&mut self, [x, y]: &[f64; 2]) {
+        self.mouse.0 = *x;
+        self.mouse.1 = *y;
+    }
+
+    pub fn mouse_click(&mut self, b: &pw::Button) {
+        if let pw::Button::Mouse(pw::MouseButton::Left) = b {
+            self.reveal(
+                (self.mouse.0 as u32) / CELL_SIZE.0,
+                (self.mouse.1 as u32) / CELL_SIZE.1,
+            );
+        }
+    }
+
+    fn reveal(&mut self, x: u32, y: u32) {
+        let c = self.cell_mut_at(x, y);
+
+        if c.hidden {
+            c.hidden = false;
+
+            if c.state == CellState::Empty {
+                for (x, y) in self.adjacent_coords(x, y).into_iter() {
+                    self.reveal(x, y);
+                }
+            }
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use failure::{format_err, Error};
+use piston_window as pw;
+
+use std::path::Path;
+
+mod game;
+use game::*;
+
+fn main() -> Result<(), Error> {
+    let mut window: pw::PistonWindow = pw::WindowSettings::new(
+        "Minesweeper",
+        (CELL_SIZE.0 * Field::SIZE.0, CELL_SIZE.1 * Field::SIZE.1),
+    )
+    .opengl(pw::OpenGL::V4_1)
+    .exit_on_esc(true)
+    .build()
+    .unwrap();
+
+    let mut field = Field::new(
+        &mut rand::thread_rng(),
+        30,
+        decode_tileset("res/tileset.jpg", &mut window.factory)?,
+    );
+
+    while let Some(e) = window.next() {
+        use pw::{MouseCursorEvent, PressEvent};
+
+        window.draw_2d(&e, |c, g| {
+            pw::clear([1.0; 4], g);
+            field.render(c, g);
+        });
+
+        if let Some(p) = e.mouse_cursor_args() {
+            field.mouse_move(&p);
+        }
+
+        if let Some(e) = e.press_args() {
+            field.mouse_click(&e);
+        }
+    }
+    Ok(())
+}
+
+fn decode_tileset<P: AsRef<Path>>(
+    p: P,
+    f: &mut pw::GfxFactory,
+) -> Result<Vec<pw::G2dTexture>, Error> {
+    use image::{jpeg::JPEGDecoder, ImageDecoder};
+    use std::fs::File;
+
+    let decoder = JPEGDecoder::new(File::open(p)?)?;
+
+    let (w, h) = {
+        let (w, h) = decoder.dimensions();
+        (w as u32, h as u32)
+    };
+    let (nx, ny) = (w / TILE_SIZE.0, h / TILE_SIZE.1);
+
+    let row_len = decoder.row_bytes() as u32;
+    let pixel_size = row_len / w;
+
+    if decoder.colortype() != image::ColorType::RGB(8) {
+        return Err(format_err!(
+            "color type not supported: {:?}",
+            decoder.colortype()
+        ));
+    }
+
+    let data = decoder.read_image()?;
+
+    let mut imbuf = image::RgbaImage::new(TILE_SIZE.0, TILE_SIZE.1);
+    let mut texvec = Vec::with_capacity((nx * ny) as usize);
+
+    for j in 0..ny {
+        for i in 0..nx {
+            let tl = (i * TILE_SIZE.0 * pixel_size, j * TILE_SIZE.1 * row_len);
+
+            for y in 0..TILE_SIZE.1 {
+                for x in 0..TILE_SIZE.0 {
+                    let py = (tl.1 + y * row_len) as usize;
+                    let px = (tl.0 + x * pixel_size) as usize;
+
+                    imbuf.put_pixel(
+                        x,
+                        y,
+                        image::Rgba([
+                            data[py + px],
+                            data[py + px + 1],
+                            data[py + px + 2],
+                            std::u8::MAX,
+                        ]),
+                    );
+                }
+            }
+
+            texvec.push(pw::Texture::from_image(
+                f,
+                &imbuf,
+                &pw::TextureSettings::new().filter(pw::Filter::Nearest),
+            )?)
+        }
+    }
+
+    Ok(texvec)
+}
